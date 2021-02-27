2645b51ab4cdfdb2ca3911b870836c85b7d4d736
marandaz <mrzwies@gmail.com>
2021-02-26 22:05:51 -0600

    fixing nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/nav";
-// import Containers from "./components/containers";
-// import Footer from "./components/footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
-// import Projects from "./pages/Projects";
-// import Resume from "./pages/Resume";
-
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+// import Footer from "./components/footer";
 
 function App() {
   return (
@@ -14,10 +12,10 @@ function App() {
       <>
         <div>
           <NavTabs />
-          <Route exact path="/react-portfolio/" component={Home} />
+          <Route exact path="/react-portfolio" component={Home} />
           <Route exact path="/react-portfolio/about" component={About} />
-          {/* <Route exact path="/react-portfolio/projects" component={Projects} /> */}
-          {/* <Route path="/react-portfolio" component={Resume} /> */}
+          <Route exact path="/react-portfolio/projects" component={Projects} />
+          <Route path="/react-portfolio" component={Resume} />
           {/* <Footer />  */}
         </div>
       </>
--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,70 +1,39 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-// import Navbar from 'react-bootstrap/Navbar';
-
-// function Nav() {
-//     const location = useLocation();
-
-//     return (
-//         <Nav className="justify-content-end" activeKey="/home">
-//             <Nav.Item>
-//                 <Link
-//                     to="/react-portfolio/about" className={location.pathname === "/react-portfolio/about" ? "nav-link active" : "nav-link"} >
-//                 </Link>
-//             </Nav.Item>
-
-    
-//         </Nav>
-//     );
-// }
-
-// export default Nav;
 
 function NavTabs(props) {
+  const location = useLocation();
+  
     return (
       <ul className="nav nav-tabs">
         <li className="nav-item">
-          <a
-            href="#home"
-            onClick={() => props.handlePageChange("Home")}
-            className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-          >
-            Home
-          </a>
-        </li>
-
-        {/* <Link
-                    to="/portfolio-react/about"
-                    className={location.pathname === "/portfolio-react/about" ? "nav-link active" : "nav-link"}
-                >
-                    about
-        </Link> */}
-        <li className="nav-item">
-          <a
-            href="#about"
-            onClick={() => props.handlePageChange("About")}
-            className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
-          >
-            About
-          </a>
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#projects"
-            onClick={() => props.handlePageChange("Projects")}
-            className={props.currentPage === "Projects" ? "nav-link active" : "nav-link"}
-          >
-            Blog
-          </a>
+        <Link
+          to="/about"
+          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        >
+          About
+        </Link>
         </li>
         <li className="nav-item">
-          <a
-            href="#resume"
-            onClick={() => props.handlePageChange("Resume")}
-            className={props.currentPage === "Resume" ? "nav-link active" : "nav-link"}
-          >
-            Contact
-          </a>
+        <Link
+          to="/projects"
+          className={location.pathname === "/projects" ? "nav-link active" : "nav-link"}
+        >
+          Projects
+        </Link>
+        </li>
+        <li className="nav-item">
+        <Link
+          to="/resume"
+          className={location.pathname === "/resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </Link>
         </li>
       </ul>
     );
